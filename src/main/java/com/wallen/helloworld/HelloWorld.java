59c069f22c022f653cddf43dda6bf887f4728eed
Wallen <872465234@qq.com>
2019-08-21 12:30:30 +0800

    2019年8月21日12:30:06-查询当前的个人任务

--- src/main/java/com/wallen/helloworld/HelloWorld.java
+++ src/main/java/com/wallen/helloworld/HelloWorld.java
@@ -4,7 +4,10 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * @Author: Wallen
@@ -28,7 +31,9 @@ public class HelloWorld {
 				.addClasspathResource("helloWorld.bpmn")
 				.addClasspathResource("helloWorld1.png")
 				.deploy();//完成部署
+		//1
 		System.out.println("部署id：" + deployment.getId());
+		//helloword入门程序
 		System.out.println("部署名称：" + deployment.getName());
 	}
 
@@ -44,7 +49,36 @@ public class HelloWorld {
 		ProcessInstance processInstance = processEngine.getRuntimeService()
 				//使用流程定义的可以启动流程实例，key对应helloworld.bpmn文件中id的属性，使用key值启动，默认是按照最新版本的流程启动
 				.startProcessInstanceByKey(processDefinitionKey);
-		System.out.println("流程实例id：" + processInstance.getId());
-		System.out.println("流程定义id" + processInstance.getProcessDefinitionId());
+		//101
+		System.out.println("流程实例Id" + processInstance.getId());
+		//helloword:1:5
+		System.out.println("流程定义ID：" + processInstance.getProcessDefinitionId());
+	}
+
+	/**
+	 * 查询当前的个人任务
+	 */
+	@Test
+	public void findMypersonalTaskTest() {
+		String assignee = "张三";
+		//与正在执行的任务相关的service
+		List<Task> list = processEngine.getTaskService()
+				//创建任务查询对象
+				.createTaskQuery()
+				//指定个人任务查询
+				.taskAssignee(assignee)
+				.list();
+		if (null != list && list.size() > 0) {
+			for (Task task : list) {
+				System.out.println("任务ID：" + task.getId());
+				System.out.println("任务名称：" + task.getName());
+				System.out.println("任务创建时间：" + task.getCreateTime());
+				System.out.println("任务办理人：" + task.getAssignee());
+				System.out.println("流程实例ID：" + task.getProcessInstanceId());
+				System.out.println("执行对象ID：" + task.getExecutionId());
+				System.out.println("流程定义ID：" + task.getProcessDefinitionId());
+				System.out.println("#########");
+			}
+		}
 	}
 }
